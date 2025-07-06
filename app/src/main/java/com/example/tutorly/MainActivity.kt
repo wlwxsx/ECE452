056623df7351bd77eccebf297c63478174a7cd8f
James S <j298song@uwaterloo.ca>
2025-07-06 12:41:16 -0400

    deleting code that is no longer referenced

--- app/src/main/java/com/example/tutorly/MainActivity.kt
+++ app/src/main/java/com/example/tutorly/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.tutorly
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,15 +12,11 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.tutorly.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
-    private lateinit var button: Button
-    private lateinit var textView: TextView
-    private lateinit var user: FirebaseUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +25,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         
         auth = FirebaseAuth.getInstance()
-        //button = findViewById(R.id.logout)
-       // textView = findViewById(R.id.user_details)
 
         //if user is not logged in, go to the login page
         val currentUser = auth.currentUser
@@ -41,16 +33,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         } else {
-            textView.text = currentUser.email
             Log.d("MainActivity", "User is logged in: ${currentUser.email}")
-        }
-        
-        //logout button
-        button.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            val intent = Intent(applicationContext, Login::class.java)
-            startActivity(intent)
-            finish()
         }
 
         //bottom navigation config
